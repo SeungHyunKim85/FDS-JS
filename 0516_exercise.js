6ee182c064e184337a90fa374abeaa0a174e0698
SeungHyunKim85 <44523619+SeungHyunKim85@users.noreply.github.com>
2019-05-16 19:00:55 +0900

    Update 0516_exercise.js

--- 0516_exercise.js
+++ 0516_exercise.js
@@ -5,7 +5,7 @@
 //
 //```js
 //circleArea(10); // 314.1592653589793
-```
+//```
 
 // 1 
 
@@ -170,33 +170,51 @@ console.log(snakeToCamel('hello_world_javascript')); // 'hello_world'
 console.log(snakeToCamel('hello_world'));
   
   
-# 문제 9
+//# 문제 9
+//
+//2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT
+//
+//입니다. 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 TUE를 반환하세요.
+//
+//제한 조건
+//
+//- 2016년은 윤년입니다.
+//- 2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
+//
+//> 출처: 프로그래머스 [2016년]
+//
+//```js
+//getDay(5, 24); // 'TUE'
+//```
 
-2016년 1월 1일은 금요일입니다. 2016년 a월 b일은 무슨 요일일까요? 두 수 a ,b를 입력받아 2016년 a월 b일이 무슨 요일인지 리턴하는 함수, solution을 완성하세요. 요일의 이름은 일요일부터 토요일까지 각각 SUN,MON,TUE,WED,THU,FRI,SAT
+// 9
 
-입니다. 예를 들어 a=5, b=24라면 5월 24일은 화요일이므로 문자열 TUE를 반환하세요.
+function getDay(a, b) {
+  const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+  const day = new Date(2016, a - 1, b).getDay();
+  return days[day];
+}
 
-제한 조건
+console.log(getDay(5, 24)); // 'TUE'
 
-- 2016년은 윤년입니다.
-- 2016년 a월 b일은 실제로 있는 날입니다. (13월 26일이나 2월 45일같은 날짜는 주어지지 않습니다)
+//# 문제 10
+//
+//전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 \*으로 가린 문자열을 리턴하는 함수를 `String.prototype.repeat`, `String.prototype.replace`, `String.prototype.substring`등의 String 관련 메소드를 활용해서 작성하세요.
+//
+//제한 조건
+//
+//- s는 길이 4 이상, 20이하인 문자열입니다.
+//
+//> 출처: 프로그래머스 [핸드폰 번호 가리기]
+//
+//```js
+//hidePhoneNumber('01033334444'); // '*******4444'
+//hidePhoneNumber('027778888'); // '*****8888'
+//```
 
-> 출처: 프로그래머스 [2016년]
+function hidePhoneNumber(phone) {
+  return '*'.repeat(phone.length - 4) + phone.substring(phone.length - 4);
+}
 
-```js
-getDay(5, 24); // 'TUE'
-```
-
-# 문제 10
-
-전화번호가 문자열 phone_number로 주어졌을 때, 전화번호의 뒷 4자리를 제외한 나머지 숫자를 전부 \*으로 가린 문자열을 리턴하는 함수를 `String.prototype.repeat`, `String.prototype.replace`, `String.prototype.substring`등의 String 관련 메소드를 활용해서 작성하세요.
-
-제한 조건
-
-- s는 길이 4 이상, 20이하인 문자열입니다.
-
-> 출처: 프로그래머스 [핸드폰 번호 가리기]
-
-```js
-hidePhoneNumber('01033334444'); // '*******4444'
-hidePhoneNumber('027778888'); // '*****8888'
+console.log(hidePhoneNumber('01033334444'));
+console.log(hidePhoneNumber('02777888'));

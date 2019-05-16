763e4b149230ec7903392fa4d9b6bd6dd280e6cc
SeungHyunKim85 <44523619+SeungHyunKim85@users.noreply.github.com>
2019-05-16 14:56:38 +0900

    Create 0516_exercise.js

---
+++ 0516_exercise.js
@@ -0,0 +1,17 @@
+// 1 
+# 문제 1
+
+양수를 입력받아 이 수를 반지름으로 하는 원의 넓이를 반환하는 함수를 작성하세요.
+
+```js
+circleArea(10); // 314.1592653589793
+```
+
+function circleArea(n) {
+  return Math.PI * Math.pow(n, 2); // Math.pow(n, 2) = n ** 2
+}
+
+console.log(circleArea(10));
+
+//2
+
